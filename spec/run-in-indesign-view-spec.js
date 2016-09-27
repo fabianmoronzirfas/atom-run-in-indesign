2ed18240ceba7c0d6f13a5c8863a9e630d105361
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2016-09-27 10:06:08 +0200

    initial commit

---
+++ spec/run-in-indesign-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RunInIndesignView from '../lib/run-in-indesign-view';
+
+describe('RunInIndesignView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
